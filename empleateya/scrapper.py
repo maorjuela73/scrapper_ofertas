3fa0d66ca85d98a7b06a3356765318bc8c61b204
Ro <errecastillo@gmail.com>
2021-01-23 11:51:52 -0500

    done : 23/01/2021 11:51:52

--- empleateya/scrapper.py
+++ empleateya/scrapper.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+import datetime
 import json
 from time import sleep
 
 
 
 def guardar(vacante , num):
-    nombre = 'vacantes/vacante_{0:04d}.json'.format(num)
+    nombre = 'vacantes/vacante_{0:03d}.json'.format(num)
     with open(nombre, 'w') as archivo_json:
         json.dump(vacante , archivo_json)
 
@@ -22,11 +23,36 @@ for j in range(0,10):
     for i in range(1,7):
         print(i)
         elem = driver.find_element_by_xpath('/html/body/app-root/app-buscar-empleo/main/article/section[1]/article[2]/div/div[{}]/div[2]'.format(i))
+        nombre_box = driver.find_element_by_xpath('/html/body/app-root/app-buscar-empleo/main/article/section[1]/article[2]/div/div[{}]/div[2]/h3'.format(i))
+        nombre = nombre_box.get_attribute('innerText')
+        try:
+            provincia_box = driver.find_element_by_xpath('/html/body/app-root/app-buscar-empleo/main/article/section[1]/article[2]/div/div[{}]/div[2]/ul/li[1]'.format(i))
+            provincia = provincia_box.get_attribute('innerText')
+        except:
+            provincia = "no especificado"
+        try:
+            jornada_box = driver.find_element_by_xpath('/html/body/app-root/app-buscar-empleo/main/article/section[1]/article[2]/div/div[{}]/div[2]/ul/li[2]'.format(i))
+            jornada = jornada_box.get_attribute('innerText')
+        except:
+            jornada = "no especificada"
+        try:
+            salario_box = driver.find_element_by_xpath('/html/body/app-root/app-buscar-empleo/main/article/section[1]/article[2]/div/div[{}]/div[2]/ul/li[3]'.format(i))
+            salario = salario_box.get_attribute('innerText')
+        except:
+            salario = 'no especificado'
+        print(nombre , provincia , jornada , salario)
         elem.click()
 
         caja = driver.find_element_by_xpath('/html/body/app-root/app-buscar-empleo/main/article/section[1]/article[2]/div/div[8]')
         caja_html =  caja.get_attribute('innerText')
-        empleo = caja_html
+        empleo = {
+            'nombre_empleo':nombre,
+            'provincia':provincia,
+            'jornada':jornada,
+            'salario':salario,
+            'descripcion_completa':caja_html,
+            'hora_scrapeada':str(datetime.datetime.now())
+        }
         guardar(empleo , total)
         afuera = driver.find_element_by_xpath('/html/body/app-root/app-buscar-empleo/main/article/section[1]/article[2]/div/div[8]/section[1]/span')
         afuera.click()
@@ -39,6 +65,7 @@ for j in range(0,10):
     sleep(0.5)
 
 
+print('done')
 driver.close()
 
 
